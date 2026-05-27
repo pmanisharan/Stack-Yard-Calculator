faafc1efb243e2a341ff98086788421d7bd13465
pmanisharan <72558998+pmanisharan@users.noreply.github.com>
2026-05-27 12:36:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const yardWidths={
+yard1:32,
+yard2:36,
+yard3:34,
+yard4:35,
+yard2$3:70
+};
+
+const cargoAngles={
+"Steam Coal":34,
+"Coking Coal":40,
+"Pet Coke":37.5,
+"Met Coke":40,
+"Bauxite":37.5
+};
+
+const yardSelect=document.getElementById("yard");
+const cargoSelect=document.getElementById("cargo");
+
+function updateWidth(){
+document.getElementById("width").value=yardWidths[yardSelect.value];
+}
+
+function updateAngle(){
+document.getElementById("angle").value=cargoAngles[cargoSelect.value];
+}
+
+yardSelect.addEventListener("change",updateWidth);
+cargoSelect.addEventListener("change",updateAngle);
+
+updateWidth();
+updateAngle();
+
+function calculate(){
+
+const yard=yardSelect.value;
+const width=parseFloat(document.getElementById("width").value);
+const density=parseFloat(document.getElementById("density").value);
+const angleDeg=parseFloat(document.getElementById("angle").value);
+const length=parseFloat(document.getElementById("length").value);
+
+if(!density || !length){
+alert("Please enter Density and Length");
+return;
+}
+
+const angleRad=angleDeg*Math.PI/180;
+
+let calculatedHeight=(length*Math.tan(angleRad))/2;
+let height=Math.min(calculatedHeight,12);
+
+document.getElementById("height").value=height.toFixed(2);
+
+let volume=0;
+
+if(yard!=="yard2$3"){
+volume=0.5*length*width*height;
+}else{
+
+const baseLength=length;
+const topLength=length-2*(height/Math.tan(angleRad));
+
+volume=((baseLength+topLength)*height*width)/2;
+
+}
+
+const qty=volume*density;
+
+document.getElementById("volume").innerHTML=volume.toFixed(2)+" m³";
+document.getElementById("qty").innerHTML=qty.toFixed(2)+" Kg";
+document.getElementById("extraNote").innerHTML=note;
+
+}
